cad0a21a5598e5dfee92f9899181394b35462697
voldemarzlg <91783549+voldemarzlg@users.noreply.github.com>
2021-10-15 15:58:56 +0000

    балдеж

---
+++ lab_1_tas_1_.py
@@ -0,0 +1,7 @@
+print("hello, world")
+print("маша + петя = любовь")
+a = 3
+b = 4
+print("x = 3+4")
+x = 3+4
+print('x =', x)
---
+++ lab_1_tas_4_.py
@@ -0,0 +1,12 @@
+a = [1, 5, "Good", "Bad"]
+b = [9, "Blue", 'Red', 11]
+c = a[1] + b[3]
+print(c)
+g = a[2] + b[2]
+print(g)
+h = a[0] * b[0]
+print(h)
+k = a[1]** b[3]
+print(k)
+p = a + b
+print(p)
